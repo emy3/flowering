b74be1edd566132cfd94564670b45b34888a05f1
emoceon@gmail.com <emoceon@gmail.com>
2024-10-03 19:29:37 +0100

    fold network and layer impl

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -6,22 +6,11 @@ pub struct Network {
 }
 
 impl Network {
-    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
-        // let mut inputs = inputs; //rebinding[shadowing]
-        // mut inputs >> appears in so-called binding position 
-        // bindings are local to the function
-        for layer in &self::layers {
-            inputs = layer.propagate(inputs);
-        }
-        inputs
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
     }
-
-    // can also write the same with the built in folds
-    // pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-    //     self.layers
-    //         .iter()
-    //         .fold(inputs, |inputs, layer| layer.propagate(inputs))
-    // }
 }
 
 #[derive(Debug)]
@@ -32,18 +21,16 @@ struct Layer {
 // numbers will have to be pushed through each layer
 impl Layer {
     pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        let mut outputs = outputs;
-        for neuro in &self.neurons {
-            let output = neuron.propagate(&inputs);
-            outputs.push(output);
-        }
-        outputs
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
     }
 }
 
 #[derive(Debug)]
 struct Neuron {
-    bias:f3,
+    bias:f32,
     weights: Vec<f32>,
 }
 
